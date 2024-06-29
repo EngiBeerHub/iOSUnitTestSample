7c122d4ce25a768ec3936af41d45c642384c25e2
EngiBeerHub <engibeer0422@gmail.com>
2024-06-29 16:08:09 +0900

    Add Pod

---
+++ Pods/Target Support Files/Pods-UnitTestSample-UnitTestSampleUITests/Pods-UnitTestSample-UnitTestSampleUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_UnitTestSample_UnitTestSampleUITests : NSObject
+@end
+@implementation PodsDummy_Pods_UnitTestSample_UnitTestSampleUITests
+@end
